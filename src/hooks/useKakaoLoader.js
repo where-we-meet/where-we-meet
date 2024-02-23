3d3d56c2adea630238a81bf43b7cf750ac2e6904
lauichan <te06230623@gmail.com>
2024-02-23 17:21:35 +0900

    [ADD] create kakaomap component

---
+++ src/hooks/useKakaoLoader.js
@@ -0,0 +1,9 @@
+import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk";
+
+export default function useKakaoLoader() {
+  const [loading, error] = useKakaoLoaderOrigin({
+    appkey: import.meta.env.VITE_APP_KAKAOMAP_API_KEY,
+    libraries: ["clusterer", "services"],
+  });
+  return [loading, error];
+}
