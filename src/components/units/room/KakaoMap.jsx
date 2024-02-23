e75037ec1eb5b5ca332b21f89e8a13e5c8991cba
heechul94 <yhcu3517@gmail.com>
2024-02-24 03:57:16 +0900

    [MOD] import halfway component

--- src/components/units/room/KakaoMap.jsx
+++ src/components/units/room/KakaoMap.jsx
@@ -1,5 +1,14 @@
 import useKakaoLoader from "@/hooks/useKakaoLoader";
 import { Map } from "react-kakao-maps-sdk";
+import Halfway from "./Halfway";
+
+const MARKERS = [
+  { lat: 33.450705, lng: 126.570677 },
+  { lat: 33.450936, lng: 126.569477 },
+  { lat: 33.451979, lng: 126.56994 },
+  { lat: 33.451393, lng: 126.570738 },
+  { lat: 33.451493, lng: 126.571018 },
+];
 
 function KakaoMap() {
   const [loading, error] = useKakaoLoader();
@@ -7,7 +16,12 @@ function KakaoMap() {
   if (loading) return <div>Loading</div>;
 
   return (
-    <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: "100%", height: "360px" }}></Map>
+    <Map
+      center={{ lat: 33.450701, lng: 126.570667 }}
+      style={{ width: "100%", height: "360px" }}
+    >
+      <Halfway markers={MARKERS} />
+    </Map>
   );
 }
 
