d34bf3def4a123184c5b6bacec774f2f4308d80a
기다림 <gidalimforsomeday@gmail.com>
2024-02-25 21:20:37 +0900

    [ADD] 토글버튼 클릭 시 맵 크기 바꾸고 레이아웃 호출연동

--- src/hooks/useKakaoLoader.js
+++ src/hooks/useKakaoLoader.js
@@ -1,9 +1,9 @@
-import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk";
+import { useKakaoLoader as useKakaoLoaderOrigin } from 'react-kakao-maps-sdk';
 
 export default function useKakaoLoader() {
   const [loading, error] = useKakaoLoaderOrigin({
     appkey: import.meta.env.VITE_APP_KAKAOMAP_API_KEY,
-    libraries: ["clusterer", "services"],
+    libraries: ['clusterer', 'drawing', 'services']
   });
   return [loading, error];
 }
--- src/pages/room/Room.jsx
+++ src/pages/room/Room.jsx
@@ -1,6 +1,8 @@
-import KakaoMap from "@/components/units/room/KakaoMap";
+// import KakaoMap from "@/components/units/room/KakaoMap";
+import MapRelayout from "@/components/units/room/MapRelayout";
 
 function Room() {
-  return <KakaoMap />;
+  // return <KakaoMap />;
+  return <MapRelayout />
 }
 export default Room;
