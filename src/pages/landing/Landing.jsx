195ed26aa321f6a9a01d52f4a27da655afffb1ea
lauichan <te06230623@gmail.com>
2024-02-26 12:58:32 +0900

    [ADD] landing layout, style

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,5 +1,15 @@
+import styles from './Landing.module.css';
+
 function Landing() {
-  return <p>Landing</p>;
+  return (
+    <section className={styles.section}>
+      <h1 className={styles.title}>Where We Meet</h1>
+      <form className={styles.form}>
+        <input></input>
+        <button>만들기</button>
+      </form>
+    </section>
+  );
 }
 
 export default Landing;
