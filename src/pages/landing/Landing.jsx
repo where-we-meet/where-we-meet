72058b68e2fbee15cccf9ea991a10683c67d6f3b
lauichan <te06230623@gmail.com>
2024-02-26 19:43:10 +0900

    [MOD] Create Room

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,11 +1,22 @@
+import { createRoom } from '@/apis/roomApi';
 import styles from './Landing.module.css';
+import { useNavigate } from 'react-router-dom';
 
 function Landing() {
+  const navigate = useNavigate();
+
+  const handleCreateRoom = async (e) => {
+    e.preventDefault();
+    const { id } = await createRoom(e.target.roomName.value);
+    navigate(`/room/${id}`);
+    e.target.reset();
+  };
+
   return (
     <section className={styles.section}>
       <h1 className={styles.title}>Where We Meet</h1>
-      <form className={styles.form}>
-        <input></input>
+      <form className={styles.form} onSubmit={handleCreateRoom}>
+        <input type="text" name="roomName"></input>
         <button>만들기</button>
       </form>
     </section>
