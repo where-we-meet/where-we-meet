3656b7601e44bcd90f1281f95120cf373e107ace
ketchup0211 <dlcka1111@naver.com>
2024-02-26 20:06:49 +0900

    [MOD] 3 sec idle and update location logic - ing

--- src/hooks/useEventTimeOut.js
+++ src/hooks/useEventTimeOut.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-function useEventTimeOut(sec, callback, event) {
+function useEventTimeOut({ sec, callback, event }) {
   useEffect(() => {
     const id = setTimeout(callback, sec * 1000);
     return () => {
