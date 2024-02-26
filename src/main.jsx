07ad6da10df8b136175bcdf637824ec7d6c80ff9
lauichan <te06230623@gmail.com>
2024-02-26 19:42:36 +0900

    [MOD] QueryClientProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
     <App />
+  </QueryClientProvider>
 );
