5b5675c287f626cb570d4b864a3342f3be2140c7
lauichan <te06230623@gmail.com>
2024-02-26 09:32:12 +0900

    [MOD] import, onZoomChanged

--- src/components/units/room/KakaoMap.jsx
+++ src/components/units/room/KakaoMap.jsx
@@ -1,13 +1,25 @@
-import useKakaoLoader from "@/hooks/useKakaoLoader";
-import { Map } from "react-kakao-maps-sdk";
+import useKakaoLoader from '@/hooks/useKakaoLoader';
+import { useState } from 'react';
+import { Map } from 'react-kakao-maps-sdk';
+import KakaoMapCircle from './KakaoMapCircle';
 
 function KakaoMap() {
   const [loading, error] = useKakaoLoader();
+  const [zoomLevel, setZoomLevel] = useState(3);
 
   if (loading) return <div>Loading</div>;
 
   return (
-    <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: "100%", height: "360px" }}></Map>
+    <Map
+      center={{ lat: 33.5563, lng: 126.79581 }}
+      style={{ width: '100%', height: '360px' }}
+      onZoomChanged={(map) => {
+        const level = map.getLevel();
+        setZoomLevel(level);
+      }}
+    >
+      <KakaoMapCircle zoomLevel={zoomLevel} />
+    </Map>
   );
 }
 
