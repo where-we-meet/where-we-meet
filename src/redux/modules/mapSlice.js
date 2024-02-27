badcf42424c462032c201a983e3f702a4e11c3ea
lauichan <te06230623@gmail.com>
2024-02-27 15:12:50 +0900

    [ADD] Circle Radius Global State

---
+++ src/redux/modules/mapSlice.js
@@ -0,0 +1,16 @@
+const initialState = {
+  radius: 0
+};
+
+const mapSlice = createSlice({
+  name: 'map',
+  initialState,
+  reducers: {
+    setCircleRadius: (state, action) => {
+      state.radius = action.payload;
+    }
+  }
+});
+
+export const { setCircleRadius } = mapSlice.actions;
+export default mapSlice.reducer;
