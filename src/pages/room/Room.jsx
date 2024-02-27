89a4f7c42524370ec5eafaa43e0087aab4a70df1
기다림 <gidalimforsomeday@gmail.com>
2024-02-27 11:15:08 +0900

    [MOD] 코드리뷰 수정

--- src/pages/room/Room.jsx
+++ src/pages/room/Room.jsx
@@ -1,4 +1,4 @@
-import { createUser, getRoomData } from '@/apis/roomApi';
+import * as roomApi from '@/apis/roomApi';
 import styles from './Room.module.css';
 import KakaoMap from '@/components/units/room/KakaoMap';
 import { useParams } from 'react-router-dom';
@@ -6,25 +6,39 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import SignInForm from '@/components/units/room/SignInForm';
 import { useState } from 'react';
 
+const persist = {
+  get: (key) => {
+    JSON.parse(sessionStorage.getItem(key));
+  },
+  set: (key, value) => {
+    sessionStorage.setItem(key, JSON.stringify(value));
+  }
+};
+
 function Room() {
   const { id } = useParams();
-  const { data, isLoading } = useQuery({ queryKey: ['room'], queryFn: () => getRoomData(id) });
-  const [currentUser, setCurrentUser] = useState(JSON.parse(sessionStorage.getItem('userInfo')));
+  const { data, isLoading } = useQuery({ queryKey: ['room'], queryFn: () => roomApi.getRoomData(id) });
+  const [currentUser, setCurrentUser] = useState(persist.get('userInfo'));
 
   const isLoggedIn = !!currentUser;
 
   const queryClient = useQueryClient();
 
   const { mutate: mutateNewUser } = useMutation({
-    mutationFn: createUser,
+    mutationFn: roomApi.createUser,
     onSuccess: () => {
       queryClient.invalidateQueries(['room']);
     }
   });
 
-  const checkExistUser = (nickname) => {
-    const selectedUser = data.users.findIndex((user) => user.nickname === nickname);
-    return selectedUser === -1 ? false : true;
+  const getExistUser = (nickname) => {
+    return data.users.find((user) => user.nickname === nickname);
+  };
+
+  const createUser = (nickname, password) => {
+    const newUser = { id: crypto.randomUUID(), nickname, pwd: password, roomId: id };
+    mutateNewUser(newUser);
+    persist.set('userInfo', newUser);
   };
 
   const handleSignIn = async (e) => {
@@ -32,22 +46,18 @@ function Room() {
     const nickname = e.target.nickname.value;
     const password = e.target.password.value;
 
-    if (checkExistUser(nickname)) {
-      const user = data.users.find((user) => user.nickname === nickname);
-
-      if (user.pwd !== password) {
-        alert('비밀번호가 틀립니다.');
-        return;
-      }
-
-      sessionStorage.setItem('userInfo', JSON.stringify(user));
-    } else {
-      const newUser = { id: crypto.randomUUID(), nickname, pwd: password, roomId: id };
-      mutateNewUser(newUser);
-
-      sessionStorage.setItem('userInfo', JSON.stringify(newUser));
+    const user = getExistUser(nickname);
+    if (user === undefined) {
+      createUser(nickname, password);
+      return;
     }
-    setCurrentUser(JSON.parse(sessionStorage.getItem('userInfo')));
+
+    if (user.pwd !== password) {
+      return alert('비밀번호가 틀립니다.');
+    }
+
+    persist.set('userInfo', user);
+    setCurrentUser(user);
   };
 
   if (isLoading) return <>Loading..</>;
@@ -64,9 +74,6 @@ function Room() {
         ) : (
           <SignInForm handleSignIn={handleSignIn} />
         )}
-        {/* {data.users.map((user) => (
-          <div key={user.id}>{user.nickname}</div>
-        ))} */}
       </section>
       <section>
         <KakaoMap />
