e696b0148a0b89ec2921b73932e681b9b213c63d
lauichan <te06230623@gmail.com>
2024-02-27 14:59:36 +0900

    [MOD] Landing Page Style

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,6 +1,7 @@
 import { createRoom } from '@/apis/roomApi';
 import styles from './Landing.module.css';
 import { useNavigate } from 'react-router-dom';
+import { FiPlusCircle } from 'react-icons/fi';
 
 function Landing() {
   const navigate = useNavigate();
@@ -14,10 +15,12 @@ function Landing() {
 
   return (
     <section className={styles.section}>
-      <h1 className={styles.title}>Where We Meet</h1>
+      <h1 className={styles.title}>Where We Meet?</h1>
       <form className={styles.form} onSubmit={handleCreateRoom}>
-        <input type="text" name="roomName"></input>
-        <button>만들기</button>
+        <input type="text" name="roomName" placeholder="모임 이름"></input>
+        <button type="button" title="모임 생성">
+          <FiPlusCircle />
+        </button>
       </form>
     </section>
   );
