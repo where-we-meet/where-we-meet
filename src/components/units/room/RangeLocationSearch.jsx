04e8d7c3335657a216b6acc9d7910f3f15b35095
ketchup0211 <dlcka1111@naver.com>
2024-02-28 18:02:41 +0900

    [ADD] range filter function semi-done

--- src/components/units/room/RangeLocationSearch.jsx
+++ src/components/units/room/RangeLocationSearch.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { MapMarker } from 'react-kakao-maps-sdk';
 import { useSelector } from 'react-redux';
 import convertGeolocationToAddress from '@/utils/convertGeolocationToAddress';
+import pin from '@assets/icons/map-pin-2-fill.png';
 
 function RangeLocationSearch() {
   const radius = useSelector((state) => state.rangeSlice.range);
@@ -13,18 +14,18 @@ function RangeLocationSearch() {
     try {
       if (center === null) return;
       const { lng, lat } = center;
-      const [result] = await convertGeolocationToAddress(center);
-      const address = result.road_address.address_name;
+      //const [result] = await convertGeolocationToAddress(center);
+      //const address = result.road_address.address_name;
       const resultList = await client.get('', {
         params: {
-          query: address,
-          x: lng,
-          y: lat,
+          query: '카페',
+          x: '' + lng,
+          y: '' + lat,
           radius: radius
         }
       });
       setRangeLocationList(resultList.data.documents);
-      console.log(rangeLocationList);
+      //console.log(rangeLocationList[0]);
     } catch (error) {
       console.error(error);
     }
@@ -34,16 +35,38 @@ function RangeLocationSearch() {
     getRangeLocationList();
   }, [radius]);
 
-  return <></>;
+  return (
+    <>
+      {rangeLocationList.map((spot) => (
+        <MapMarker
+          key={spot.id}
+          position={{ lng: spot.x, lat: spot.y }}
+          image={{
+            src: pin,
+            size: {
+              width: 15,
+              height: 15
+            },
+            options: {
+              offset: {
+                x: 8,
+                y: 16
+              }
+            }
+          }}
+        />
+      ))}
+    </>
+  );
 }
 
 export default RangeLocationSearch;
 
 //  1. 범위 내 장소 리스트 받아오기
-//    - 중심 좌표 받아오기
-//    - 범위 받아오기
-//    - 좌표로 주소 변환하기
-//    - 변환한 주소 기반으로 키워드로 장소 검색하기 API 사용하여 요청
-//    - 받아온 장소 리스트를 지역 상태로 관리
+//    - 중심 좌표 받아오기 (ok)
+//    - 범위 받아오기 (ok)
+//    - 좌표로 주소 변환하기 (ok)
+//    - 변환한 주소 기반으로 키워드로 장소 검색하기 API 사용하여 요청 (ok)
+//    - 받아온 장소 리스트를 지역 상태로 관리 (ok)
 //  2. 장소 리스트들에 대한 마커 생성하기
 //  3. 마커 표시하기
