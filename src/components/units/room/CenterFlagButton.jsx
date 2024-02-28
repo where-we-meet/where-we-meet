682c13e4b8d71efb3c8e6208a43490db321b7a33
ketchup0211 <dlcka1111@naver.com>
2024-02-28 15:13:00 +0900

    [MOD] modify centerflagbutton UI

--- src/components/units/room/CenterFlagButton.jsx
+++ src/components/units/room/CenterFlagButton.jsx
@@ -1,7 +1,13 @@
 import style from './CenterFlagButton.module.css';
+import { FaFlag } from 'react-icons/fa';
 
 function CenterFlagButton() {
-  return <button>위치로</button>;
+  return (
+    <button className={style.button}>
+      <FaFlag className={style.button_icon} />
+      <p className={style.button_text}>중간 지점으로 이동</p>
+    </button>
+  );
 }
 
 export default CenterFlagButton;
