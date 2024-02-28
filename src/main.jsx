ccef9021926e2347c87f2c47b843ceb42253c998
lauichan <te06230623@gmail.com>
2024-02-28 09:54:54 +0900

    [MOD] import redux store

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
+import store from './redux/config/configStore';
 
 export const queryClient = new QueryClient();
 
