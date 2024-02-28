015fbb699a4161b50fa6577a548dff46bf9f9f9c
heechul94 <yhcu3517@gmail.com>
2024-02-28 16:27:24 +0900

    [MOD] modify convexHull algorithm logic

--- src/utils/convexHull.js
+++ src/utils/convexHull.js
@@ -1,32 +1,43 @@
-function ccw(a, b, c) {
-  return (
-    (b.location.lat - a.location.lat) * (c.location.lng - a.location.lng) -
-    (c.location.lat - a.location.lat) * (b.location.lng - a.location.lng)
-  );
-}
-
 export default function convexHull(points) {
-  // 가장 아래에 있는 점을 찾음
-  if (points.length <= 1) return;
+  points.sort((a, b) => a.location.lng - b.location.lng || a.location.lat - b.location.lat);
 
-  const lowestPoint = points.reduce((lowest, { location: point }) => {
-    return point.lng < lowest.lng ? point : lowest;
-  });
+  const upperHull = [];
+  const lowerHull = [];
 
-  // lowestPoint를 기준으로 점들을 각도순으로 정렬
-  points.sort(({ location: a }, { location: b }) => {
-    const angleA = Math.atan2(a.lng - lowestPoint.location.lng, a.lat - lowestPoint.location.lat);
-    const angleB = Math.atan2(b.lng - lowestPoint.location.lng, b.lat - lowestPoint.location.lat);
-    return angleA - angleB;
-  });
-  const stack = [points[0], points[1]];
-
-  for (let i = 2; i < points.length; i++) {
-    let top = stack.pop();
-    while (ccw(stack[stack.length - 1], top, points[i]) <= 0) {
-      top = stack.pop();
+  for (const p of points) {
+    while (upperHull.length >= 2) {
+      const q = upperHull[upperHull.length - 1];
+      const r = upperHull[upperHull.length - 2];
+      if (
+        (q.location.lng - r.location.lng) * (p.location.lat - r.location.lat) >=
+        (q.location.lat - r.location.lat) * (p.location.lng - r.location.lng)
+      ) {
+        upperHull.pop();
+      } else {
+        break;
+      }
     }
-    stack.push(top, points[i]);
+    upperHull.push(p);
   }
-  return stack;
+
+  for (let i = points.length - 1; i >= 0; i--) {
+    const p = points[i];
+    while (lowerHull.length >= 2) {
+      const q = lowerHull[lowerHull.length - 1];
+      const r = lowerHull[lowerHull.length - 2];
+      if (
+        (q.location.lng - r.location.lng) * (p.location.lat - r.location.lat) >=
+        (q.location.lat - r.location.lat) * (p.location.lng - r.location.lng)
+      ) {
+        lowerHull.pop();
+      } else {
+        break;
+      }
+    }
+    lowerHull.push(p);
+  }
+
+  upperHull.pop();
+  lowerHull.pop();
+  return upperHull.concat(lowerHull);
 }
