0df71cd927ea7c042386c15a95341afc0db16fdd
ketchup0211 <dlcka1111@naver.com>
2024-02-28 15:26:45 +0900

    [ADD] move center point

--- src/components/units/room/KakaoMap.jsx
+++ src/components/units/room/KakaoMap.jsx
@@ -4,12 +4,14 @@ import { Map } from 'react-kakao-maps-sdk';
 import KakaoMapCircle from './KakaoMapCircle';
 import Halfway from './Halfway';
 import styles from './KakaoMap.module.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import CenterFlagButton from './CenterFlagButton';
+import { setViewPoint } from '@/redux/modules/mapSlice';
 
 function KakaoMap() {
   const [loading, error] = useKakaoLoader();
   const [zoomLevel, setZoomLevel] = useState(3);
+  const dispatch = useDispatch();
 
   const viewPoint = useSelector((state) => state.mapSlice.viewPoint);
 
@@ -23,6 +25,11 @@ function KakaoMap() {
         onZoomChanged={(map) => {
           const level = map.getLevel();
           setZoomLevel(level);
+        }}
+        onDragEnd={(map) => {
+          const latlng = map.getCenter();
+          const currentLatLng = { lat: latlng.getLat(), lng: latlng.getLng() };
+          dispatch(setViewPoint(currentLatLng));
         }}
       >
         <Halfway />
